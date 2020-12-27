b79fbb85b9faa3946ff8c23f38bb553c06b10da9
jaypii <jpomv2@icloud.com>
2020-12-27 14:04:17 +0100

    Node Project init 201227

--- nodejs-express-sequelize-mysql/server.js
+++ nodejs-express-sequelize-mysql/server.js
@@ -4,6 +4,12 @@ const cors = require("cors");
 
 const app = express();
 
+const db = require("./app/models");
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log("Drop and re-sync db.");
+});
+
 var corsOptions = {
   origin: "http://localhost:8081"
 };
@@ -16,19 +22,13 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Synch DB
-const db = require("./models");
-db.sequelize.sync()
-
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the tutorial API" });
+  res.json({ message: "Welcome to bezkoder application." });
 });
 
-require("./routes/tutorial.routes")(app);
-
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(process.env.PORT, () => {
+const PORT = process.env.PORT || 8081;
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
