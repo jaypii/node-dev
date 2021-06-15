3e9fd8e67d9f02c263e4e0856904f44f9e4fb057
jaypii <jpomv2@icloud.com>
2021-06-15 15:32:53 +0200

    Angular update to 12.x

--- ng11-js-client-crud/src/app/app.component.ts
+++ ng11-js-client-crud/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng10-js-client-crud';
+  title = 'ng12-js-client-crud';
   todayDate = new Date();
 }
