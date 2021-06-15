1b058cf11cc5a111d12283c5a7955d98aad4a0c6
jaypii <jpomv2@icloud.com>
2021-06-15 10:17:50 +0200

    Vue 3 Update

--- vue-js-client-crud/src/main.js
+++ vue-js-client-crud/src/main.js
@@ -1,10 +1,8 @@
-import Vue from 'vue'
+
+import { createApp } from 'vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).use(router).mount('#app')
