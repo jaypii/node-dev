93026892f8f9f661eadefbb4519413ae382b9a35
jaypii <jpomv2@icloud.com>
2021-07-01 15:38:28 +0200

    Modifications 210701

--- nodejs-express-sequelize-mysql/server.js
+++ nodejs-express-sequelize-mysql/server.js
@@ -21,7 +21,7 @@ db.sequelize.sync();
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome" });
 });
 
 require("./app/routes/tutorial.routes")(app);
