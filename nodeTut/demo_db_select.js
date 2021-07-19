9b1fe763fd10145aba937b6c30399bc3d0b295d1
jaypii <jpomv2@icloud.com>
2021-07-19 14:16:18 +0200

    modif 210719

--- nodeTut/demo_db_select.js
+++ nodeTut/demo_db_select.js
@@ -9,8 +9,9 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) throw err;
-  con.query("SELECT name,race,startclass,lvltotal FROM nwn2chars WHERE race LIKE 'Human'", function (err, result, fields) {
+  con.query("SELECT name,race,startclass,lvltotal FROM nwn2chars WHERE race LIKE 'Human' ORDER BY lvltotal", function (err, result, fields) {
     if (err) throw err;
+    console.log('===== HUMAN CHARACTERS sorted by Levels =====\n')
     console.log(result);
   });
 });
