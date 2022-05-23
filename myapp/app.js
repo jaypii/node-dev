7a97521c482018736aeb2f8f117f0abbefb5afde
jaypii <jpomv2@icloud.com>
2022-05-23 14:40:28 +0200

    Node Modif 220523

--- myapp/app.js
+++ myapp/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const port = 3000
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -26,6 +28,8 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
